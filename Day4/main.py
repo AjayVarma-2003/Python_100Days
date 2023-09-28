82b65688cb7925ffe9c93e999212972f0a535df7
AjayVarma-2003 <123473980+AjayVarma-2003@users.noreply.github.com>
2023-09-28 20:44:35 +0530

    Add files via upload

---
+++ Day4/main.py
@@ -0,0 +1,22 @@
+# import random
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+# random_float = random.random()
+# print(random_float)
+
+# List = ["item1","item2","itme3","...."]
+# List[1] = "HUIHUI"
+# print(List)
+# print(List[0])
+# print(List[1])
+# print(List[2])
+
+# fruits = ['Strawberries','Nectarines','Apples','Grapes','Peaches','Cherries','Pears']
+# vegetables = ['Spinach','Kale','Tomatoes','Celery','Potatoes']
+
+# dirty_dozen = [fruits, vegetables]
+# print(dirty_dozen)
+
+# print(dirty_dozen[1][1])
