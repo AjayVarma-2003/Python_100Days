09fdfea5a3537d0a63976e4116fc44c6f90838f1
AjayVarma-2003 <ajayvarma2003@gmail.com>
2023-10-02 11:55:23 +0530

    Initial Commit

---
+++ Day5/main.py
@@ -0,0 +1,11 @@
+# fruits = ['Apple','Peach','Pear']
+# for fruit in fruits:
+#     print(fruit)
+
+# for number in range(1, 10, 3):  #3 is step of printing
+#     print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+    print(total)
