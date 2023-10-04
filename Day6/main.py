6375546c47fb59fe659f2ce0f2796aad1d59caaa
AjayVarma-2003 <123473980+AjayVarma-2003@users.noreply.github.com>
2023-10-04 10:30:21 +0530

    Add files via upload

---
+++ Day6/main.py
@@ -0,0 +1,11 @@
+# def my_function():
+#     print("Heloo")
+#     print("Bye")
+# my_function()
+
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
+
